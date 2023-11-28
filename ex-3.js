b977de12e1bd77eb04a617f132b1dddb44e3e1dd
Havocrazer <92991759+Havocrazer@users.noreply.github.com>
2023-11-28 17:13:49 +0700

    feat: add ex1-6

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,19 @@
-// Start coding here
+let add = (number_a, number_b) => {
+  const result = number_a + number_b;
+  return result;
+};
+
+let substract = (number_a, number_b) => {
+  const result = number_a - number_b;
+  return result;
+};
+
+let multiply = (number_a, number_b) => {
+  const result = number_a * number_b;
+  return result;
+};
+
+let divide = (number_a, number_b) => {
+  const result = number_a / number_b;
+  return result;
+};
