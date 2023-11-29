9071853978a388c15bc256dae785a75354af459f
Chayutp <135034854+Chayutp@users.noreply.github.com>
2023-11-29 12:22:48 +0700

    feat:(js-functions)phum

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,16 @@
 // Start coding here
+let add = (a, b) => a + b;
+
+add(10, 5);
+
+let subtract = (a, b) => a - b;
+
+subtract(10, 5);
+
+let multiply = (a, b) => a * b;
+
+multiply(10, 5);
+
+let divide = (a, b) => a / b;
+
+divide(10, 5);
